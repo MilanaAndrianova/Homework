3234edf5e3d59b3999fcaede9525538703eae2ae
MilanaAndrianova <mila89821698005@mail.ru>
2024-07-18 15:51:49 +0500

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,5 @@
+course='Python'
+first_number=12
+second_number=1.5
+third_number=0.125
+print('курс:',course,',всего задач:',first_number,',затрачено часов:',second_number,',средние время выполнения:',third_number,'часа')
